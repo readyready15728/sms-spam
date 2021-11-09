52cba1a47ed627a1cce85262d5eeafa61e57788a
Lynn Bradshaw <readyready15728@gmail.com>
2021-11-08 22:57:49 -0500

    Changing model to kernlab SVM and implementing save feature

--- learn.R
+++ learn.R
@@ -1,5 +1,4 @@
 library(ROSE)
-library(discrim)
 library(textrecipes)
 library(tidymodels)
 library(tidyverse)
@@ -26,30 +25,40 @@ sms_recipe <- recipe(class ~ text, data=sms_balanced_training) %>%
 # Create workflow
 sms_workflow <- workflow() %>% add_recipe(sms_recipe)
 
-# Create naïve Bayes specification 
-nb_specification <- naive_Bayes() %>%
+# Create SVM specification 
+svm_specification <- svm_rbf() %>%
   set_mode('classification') %>%
-  set_engine('naivebayes')
-
-# Fit training data
-nb_fit <- sms_workflow %>%
-  add_model(nb_specification) %>%
-  fit(data=sms_balanced_training)
+  set_engine('kernlab')
 
 # Create cross validation folds
 set.seed(42)
 sms_folds <- vfold_cv(sms_balanced_training)
 
 # Create new workflow for CV
-nb_workflow <- workflow() %>%
+svm_workflow <- workflow() %>%
   add_recipe(sms_recipe) %>%
-  add_model(nb_specification)
+  add_model(svm_specification)
 
-# Fit resampled CV folds
-nb_resampled <- fit_resamples(
-  nb_workflow,
-  sms_folds,
-  control=control_resamples(save_pred=TRUE)
-)
+# Fit resampled CV folds then save or load existing file
+fit_path = 'fit.rds'
 
-print(nb_resampled)
+if (file.exists(fit_path)) {
+  svm_resampled <- readRDS(fit_path)
+} else {
+  options(tidymodels.dark=TRUE) # So I can actually what is going on
+  svm_resampled <- fit_resamples(
+    svm_workflow,
+    sms_folds,
+    control=control_resamples(save_pred=TRUE, verbose=TRUE)
+  )
+
+  saveRDS(svm_resampled, 'fit.rds')
+}
+
+# Evaluate performance on training set
+svm_resampled_metrics <- collect_metrics(svm_resampled)
+svm_resampled_predictions <- collect_predictions(svm_resampled)
+
+# Don't forget to try it with unbalanced original
+print(svm_resampled_metrics)
+print(svm_resampled_predictions)
